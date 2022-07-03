3fc4cbea48bde04b8c2e2ba3303ff407beb68544
Vinayak Shrivastava <75775994+Vinayak2002@users.noreply.github.com>
2022-07-04 00:52:28 +0530

    Update settings.py

--- Hackmanthan/settings.py
+++ Hackmanthan/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "" # Make sure to add the secret key before running the application
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
